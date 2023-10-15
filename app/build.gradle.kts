637ce065dcdf4b6871565911bcec0530792220b9
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2023-10-14 23:21:25 -0300

    remove project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,9 +36,6 @@ android {
 }
 
 dependencies {
-
-    implementation(project(":progress-view"))
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.6.1")
